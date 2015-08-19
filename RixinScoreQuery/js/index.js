dbc7773d6aec38c7d942e621b5d0ca54f23f7044
CreatorLanslot <creatorlanslot@gmail.com>
2015-08-19 23:17:57 +0800

    修复了已知bug 修改了一些细节

--- RixinScoreQuery/js/index.js
+++ RixinScoreQuery/js/index.js
@@ -7,6 +7,17 @@
 
 $(document).ready(function(){
 
+    var TermList = [
+        ["2012-2013学年 第一学期", 2012.1], 
+        ["2012-2013学年 第二学期", 2012.2],
+        ["2013-2014学年 第一学期", 2013.1], 
+        ["2013-2014学年 第二学期", 2013.2], 
+        ["2014-2015学年 第一学期", 2014.1],
+        ["2014-2015学年 第二学期", 2014.2],
+        ["2015-2016学年 第一学期", 2015.1],
+        ["2015-2016学年 第二学期", 2015.2],
+    ]
+
     // 初始化查询按钮位置
     var initialBtn = setInterval(function () {
         var button = $("#get-score"),
@@ -18,7 +29,17 @@ $(document).ready(function(){
     }, 16);
 
     $("#get-score").click(function() {
-        var score = $(".student-id-input").val();
+        var score = $(".student-id-input").val(),
+            term = $(".select_showbox").text();
+            // console.log(term);
+        var temp;
+        for (var i = 0; i < TermList.length; i++) {
+            if (TermList[i][0] == term) {
+                temp = TermList[i][1];
+                // console.log(temp);
+            };
+        };
+        // console.log(term);
         // console.log(tyepof score, score);
         var URLRixin = "http://api.ecjtu.net/score.php",
             URLEcjtu = "http://jwc.ecjtu.jx.cn/mis_o/cj.php?sid=";        
@@ -29,21 +50,31 @@ $(document).ready(function(){
             data: {s: score},
             success: function (json) {
                 // 初始化展示列表
+                var count = 0;
                 $("ul.show").empty()
                     .append("<li class='show-left table-head top-border right-border bottom-border'>课程名称</li><li class='show-right table-head top-border bottom-border'>分数</li>");
 
                 // 获取数据并添加元素
                 $.each(json, function(i, scoreInfo){
+                    
                     // console.log(scoreInfo.Term);
-                    if(scoreInfo.Term != 2014.1 ) return true;
-                    $("ul.show").append("<li class='show-left table-cell course'>" + scoreInfo.Course + "</li><li class='show-right table-cell course-score'>" + scoreInfo.Score + "</li>");
-                    // console.log(scoreInfo);
+                    if(scoreInfo.Term == temp ) {
+                        // console.log(scoreInfo.Name);
+                        $("input.name-input").val(scoreInfo.Name)
+                        $("ul.show").append("<li class='show-left table-cell course'>" + scoreInfo.Course + "</li><li class='show-right table-cell course-score'>" + scoreInfo.Score + "</li>");
+                        count ++;
+                    }
                 });
+
+                if (count == 0) {
+                    $("ul.show").empty()
+                        .append("<p class='empty'> 没有查到成绩咕OuO~ </p>");
+                };
 
                 // 不及格成绩标记
                 $("ul.show").children('li.course-score').each(function() {
                     if ($(this).text() < 60 || $(this).text() == "不及格" || $(this).text() == "不合格") {
-                        console.log("X");
+                        // console.log("X");
                         $(this).css('background', '#f96')
                             .prev().css('background', '#f96');
                     };
